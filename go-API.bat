85abfa0e64e14f1b9d69717df3a4379c1524e337
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2025-09-09 15:08:45 +0200

    *

---
+++ go-API.bat
@@ -0,0 +1,5 @@
+
+set HF_HOME=%CD%\checkpoints
+
+indextts2runtime\python.exe indextts2_api_compatible.py
+pause
---
+++ go-webui.bat
@@ -0,0 +1,5 @@
+
+set HF_HOME=%CD%\checkpoints
+
+indextts2runtime\python.exe webui.py --host 127.0.0.1
+pause
