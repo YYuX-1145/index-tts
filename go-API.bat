8eb58b695435cb191b0105e39827cc3c09f613ab
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2025-09-12 23:15:54 +0200

    Update go-API.bat

--- go-API.bat
+++ go-API.bat
@@ -1,5 +1,5 @@
 
 set HF_HOME=%CD%\checkpoints
 
-indextts2runtime\python.exe indextts2_api.py --use_deepspeed --cuda_kernel
+indextts2runtime\python.exe indextts2_api.py --deepspeed --cuda_kernel
 pause
